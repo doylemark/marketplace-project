bd376f07db240a215795723db9a46783fcd4070c
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2024-02-23 12:20:17 +0000

    impl SeederService

---
+++ src/main/java/com/markdoyle/marketplace/services/SeederService.java
@@ -0,0 +1,32 @@
+package com.markdoyle.marketplace.services;
+
+import java.io.File;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.markdoyle.marketplace.entities.Car;
+import com.markdoyle.marketplace.repositories.CarRepository;
+
+@Service
+public class SeederService implements CommandLineRunner {
+    @Autowired
+    CarRepository carRepository;
+
+    public void run(String... args) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            List<Car> cars = mapper.readValue(new File("./data/seed.json"), new TypeReference<List<Car>>() {
+            });
+            carRepository.saveAll(cars);
+        } catch (Exception err) {
+            System.err.println("Failed to initialize car repository with seed data");
+        }
+    }
+
+}
